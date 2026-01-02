ac558740770de16e7fc7ed8bb6992cbd036d9628
Micfri <44413144+mikefri@users.noreply.github.com>
2026-01-02 21:25:18 +0100

    Update save-hours.js

--- api/save-hours.js
+++ api/save-hours.js
@@ -2,14 +2,15 @@ import { kv } from '@vercel/kv';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Méthode non autorisée' });
-  
+
   const { hourOn, hourOff } = req.body;
-  
+
   try {
-    // On enregistre les heures dans ta base Redis de Paris
+    // On enregistre les deux valeurs dans la base de données
     await kv.set('hour_on', hourOn);
     await kv.set('hour_off', hourOff);
-    return res.status(200).json({ success: true });
+
+    return res.status(200).json({ success: true, msg: 'Planning enregistré !' });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
